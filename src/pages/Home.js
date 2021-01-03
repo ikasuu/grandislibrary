edb7f75766149980bb47a7c1690b14f0a9edf580
ikasuu <71949537+ikasuu@users.noreply.github.com>
2021-01-02 18:45:36 -0800

    Home grid items made more mobile friendly
    home-content.json moved elsewhere
    Made home a functional component instead again!!
    home-content.json is now imported instead of fetched

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
+
+import data from '../special/home-content.json';
 
 import FeaturedSwiper, { ContentSwiper } from '../components/HomepageSwiper';
 import { TopClassSwipe } from '../components/ClassSwipe';
 import GridContainer from '../components/GridContainer';
-import HeaderImage from '../components/HeaderImage';
-import { HeaderImageUrl } from '../special/Values';
 
 // Import CSS
 import '../css/homepage.css';
@@ -15,54 +15,22 @@ import '../css/homepage.css';
     Created by: Ikasuu, Fall 2020
 */
 
-export class Home extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-        data: {},
-        loading: true,
-        fail: false
-    };
-}
-
-  //Once page is mounted, fetch the contents used for the Swiper components such as Recent News, Upcoming Updates, etc
-  componentDidMount(){
-    fetch("./data/home-content.json")
-    .then(res => res.json())
-    .then(fetchedData => this.setState({data: fetchedData, loading: false, fail: false}))
-    .catch(err => this.setState({fail: true}));
-  }
-
-  render() {
-    const { loading, fail, data } = this.state ;
-    return (
-      <div>
-        {
-          loading ? 
-              <div>
-                <HeaderImage imageUrl={`${HeaderImageUrl.library}.jpg`}/>
-                <Container>{fail ? <h3>Looks like there was an error in the URL you entered,
-                  the page you are looking for may be moved or deleted.</h3> : ""}</Container>
-              </div> :
-          <div style={{marginTop: '-1rem'}}>
-            <GridContainer/>
-            <Container>
-              <FeaturedSwiper content={data.featured}/>
-              <h2>Recent News in Global MapleStory</h2>
-              <ContentSwiper content={data.recent}/>
-              <h2>Upcoming Updates</h2>
-              <ContentSwiper content={data.upcoming}/>
-              <h2>Popular Content</h2>
-              <h2>Last Month's Most Viewed Classes</h2>
-              <TopClassSwipe classes={data.popularClasses}/>
-            </Container>
-          </div>
-        }
-      </div>
-    )
-  }
+function Home(){
+  return(
+    <div style={{marginTop: '-1rem'}}>
+      <GridContainer/>
+      <Container>
+        <FeaturedSwiper content={data.featured}/>
+        <h2>Recent News in Global MapleStory</h2>
+        <ContentSwiper content={data.recent}/>
+        <h2>Upcoming Updates</h2>
+        <ContentSwiper content={data.upcoming}/>
+        <h2>Popular Content</h2>
+        <h2>Last Month's Most Viewed Classes</h2>
+        <TopClassSwipe classes={data.popularClasses}/>
+      </Container>
+    </div>
+  );
 }
 
 export default Home
