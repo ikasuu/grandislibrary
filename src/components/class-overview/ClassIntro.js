8a9a5088416e5f54f37f4a6c1f5dab352109a2fe
Ikasuu <71949537+ikasuu@users.noreply.github.com>
2026-06-17 15:42:38 -0700

    Update ClassIntro.js

--- src/components/class-overview/ClassIntro.js
+++ src/components/class-overview/ClassIntro.js
@@ -385,7 +385,7 @@ function BoostNodeLegend(){
                     </div>
                     <div className="bs-data-vals">
                         <ul><li>OTHER</li></ul>
-                        <div>Boost Nodes that do not need to be upgraded</div>
+                        <div>Boost Nodes that do not need to be upgraded or has least priority</div>
                     </div>
                 </Container>
             </AccordionDetails>
